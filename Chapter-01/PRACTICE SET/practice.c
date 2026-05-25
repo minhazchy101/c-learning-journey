c0199f58d00df1622e1726bf841e6a5592abcf55
minhazchy101 <minhazchowdhury101@gmail.com>
2026-05-25 13:54:34 +0600

    Day 01: C basics - variables, input/output, arithmetic operations

---
+++ Chapter-01/PRACTICE SET/practice.c
@@ -0,0 +1,66 @@
+// P-01(a)
+/*#include <stdio.h>
+
+int main(){
+    int length = 8;
+    int width = 5;
+    printf("The area %d", length*width);
+    return 0;
+}*/
+
+// P-01(b)
+/*
+#include <stdio.h>
+
+int main(){
+    int length;
+    int width;
+    printf("Enter the length : ");
+    scanf("%d", &length);
+    printf("Enter the width : ");
+    scanf("%d", &width);
+    printf("The area %d", length*width);
+   
+    return 0;
+}*/
+
+// P-02
+/*
+#include <stdio.h>
+
+// #define _USE_MATH_DEFINES
+
+#include <math.h>
+int main(){
+    int r = 3;
+   float pie = 3.1416;
+    // printf("the Area of a circle: %f", pie*r*r);
+    float a = (pie*r*r);
+
+    // float a = (M_PI*r*r);
+
+    int h = 10;
+    printf("the volume of a cylinder: %f", a*h);
+    return 0;
+}*/
+
+// P-03
+// #include <stdio.h>
+
+// int main(){
+//     float c = 25, f;
+//     f = (9.0/5.0)*c+32;
+//     printf("Convert C to Fahrenheit: %f", f);
+//     return 0;
+// }
+
+// P-04
+/*
+#include <stdio.h>
+
+int main(){
+    float p = 1000, r=5, t=2;
+    printf("the simple interest: %f", (p*r*t)/100);
+    return 0;
+}
+    */
---
+++ Chapter-01/index.c
@@ -0,0 +1,46 @@
+
+// printf
+/*#include <stdio.h>
+
+int main(){
+    printf("Hello");
+    return 0;
+} */
+
+// VARIABLES
+/*#include <stdio.h>
+
+int main(){
+    int a;
+    a= 20;
+   // printf("%d", a);
+    int b = 21;
+   //printf("%d", b);
+
+   char c = 'c';
+  // printf("%c", c);
+
+   float f = 3.1416;
+  // printf("%f", f);
+
+    return 0;
+}*/
+
+// #include <stdio.h>
+
+// int main(){
+//      printf("Hello,\n I am learning  C \t in VS CODE"); 
+//     return 0;
+// }
+
+//RECEIVING INPUT 
+#include <stdio.h>
+
+int main(){
+    int i;
+    printf("Enter the number : ");
+    scanf("%d", &i);
+    printf("the number: %d", i);
+
+    return 0;
+}
