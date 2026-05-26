ed35372f0a5ffd30f544f81d75d5ee1ba337030e
minhazchy101 <minhazchowdhury101@gmail.com>
2026-05-26 21:24:53 +0600

    Day 02: Chapter 02 - practiced type conversion, arithmetic expressions, and operator precedence in C

---
+++ Chapter-02/PRACTICE SHET/practice.c
@@ -0,0 +1,42 @@
+
+/*
+#include <stdio.h>
+
+int main(){
+    float f = 3.0;
+    int i = 8;
+    printf("%f", f/i - 2);
+    return 0;
+}
+    */
+
+  /*#include <stdio.h>
+  
+  int main(){
+    int a = 2341;
+    // int a = 194;
+    printf("the value of a%97 is %d", a%97);
+    return 0;
+  }  
+*/
+
+/* #include <stdio.h>
+
+int main(){
+    int x=2, y=3, z=3, k=1;
+    int a = 3*x;
+    int b = a/y;
+    int c = b - z;
+    int d = c + k;
+    int e = 3*x/y - z+k;
+    printf("the result %d is total %d", d, e);
+    return 0;
+}*/
+
+#include <stdio.h>
+
+int main(){
+    float a = 3.0 + 1;
+    printf("the type : %f", a);
+    return 0;
+}
---
+++ Chapter-02/index.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+   /*  int i = 10;
+    int j = i;
+     printf("the value of i is %d and value of j %d", i,j);
+    */
+
+   /* int a = 4;
+    int b = 3;
+    int c= a + b;
+    printf("the value of a is %d and value of b is %d and sum is %d\n", a,b,c);
+
+    // //Modulus
+    printf("the remainder when a is a divided by b is %d", a%b);*/
+
+    /*int a = 4;
+    float a = 4.1;
+    int b = 3;
+    float c = a/b;
+    printf("the value of the a/b is %f", c);*/
+
+   /* int a = 1.2;
+    printf("the value of a is %d", a);
+    float a = 1;
+    printf("the value of a is %f", a);*/
+
+  /* int a = 3;
+    int b = 6;
+    int c = 9;
+      printf("the value is %d", a*b/c);
+     printf("the value is %d", a*b/c + 7);
+    printf("the value is %d", 3*a - 8*b);*/
+
+   /* int a;
+    float f = 32.1;
+    a = (int) f;
+    printf("%d", a);
+
+    return 0;*/
+}
